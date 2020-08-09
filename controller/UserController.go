f1c953372bd21b2555c06f2f8bd47f01cd2acab9
BonjourGo <1510298763@qq.com>
2020-08-09 15:24:34 +0800

    login

--- controller/UserController.go
+++ controller/UserController.go
@@ -7,10 +7,11 @@ import (
 	"ginEssential/utils"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
+	"golang.org/x/crypto/bcrypt"
 	"net/http"
 )
 
-func Resgister(c *gin.Context)  {
+func Register(c *gin.Context)  {
 	db := common.DB
 	// 获取参数
 	name := c.PostForm("name")
@@ -44,11 +45,18 @@ func Resgister(c *gin.Context)  {
 		name = utils.RandString(10)
 	}
 	// 加密密码
-	//screctPasspord, err := Gen
+	password, err := bcrypt.GenerateFromPassword([]byte(pwd), bcrypt.DefaultCost)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"code": 500,
+			"msg":  "手机号已存在，请登录！",
+		})
+		return
+	}
 	newUser := model.User{
 		Name:  name,
 		Phone: phone,
-		Pwd:   pwd,
+		Pwd:   string(password),
 	}
 	db.Create(&newUser)
 	c.JSON(http.StatusOK, gin.H{
@@ -56,6 +64,52 @@ func Resgister(c *gin.Context)  {
 		"msg":  "注册成功",
 	})
 	fmt.Println(name, phone, pwd)
+}
+
+func Login(c *gin.Context)  {
+	DB := common.GetDB()
+	// 获取参数
+	phone := c.PostForm("phone")
+	pwd := c.PostForm("pwd")
+	// 验证数据
+	//if len(pwd) < 6 {
+	//	c.JSON(http.StatusUnprocessableEntity, gin.H{
+	//		"code": 422,
+	//		"msg":  "密码必须为6位",
+	//	})
+	//	return
+	//}
+	if len(phone) != 11 {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code": 422,
+			"msg":  "手机号必须为11位",
+		})
+		return
+	}
+
+	var user model.User
+	DB.Where("phone = ?", phone).First(&user)
+	if user.ID == 0 {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code": 422,
+			"msg":  "手机号未注册！",
+		})
+		return
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Pwd), []byte(pwd)); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"code": 400,
+			"msg":  "密码错误！",
+		})
+		return
+	}
+	token := "ok"
+	c.JSON(http.StatusOK, gin.H{
+		"code": 200,
+		"data": gin.H{"token":token},
+		"msg":  "登陆成功",
+	})
 }
 
 func isPhoneExist(db *gorm.DB, phone string) bool {
--- routers.go
+++ routers.go
@@ -6,6 +6,7 @@ import (
 )
 
 func GetRouter(r *gin.Engine) *gin.Engine {
-	r.POST("/register", controller.Resgister)
+	r.POST("/register", controller.Register)
+	r.POST("/login", controller.Login)
 	return r
 }
